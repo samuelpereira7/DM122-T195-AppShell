f327ff64345b9847a3e7f9b3eca46a229add9bdd
Edy Segura <edysegura@gmail.com>
2020-11-21 11:52:23 -0300

    Add other SW life cycles

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing service worker...');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] activating service worker...');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('[Service Worker] fetch event...', event);
 });
