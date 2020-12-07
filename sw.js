cf3ca7e04f6dfc55475904c6bf457a6f014cfe61
Samuel Pereira <samuelpereira@gec.inatel.br>
2020-12-06 21:58:14 -0300

    add cache cleanup

--- sw.js
+++ sw.js
@@ -1,25 +1,47 @@
+const cacheName = 'app-shell-v1';
+const assetsToCache = [
+  'offline.html',
+  'index.html',
+];
+
+async function cacheStaticAssets() {
+  try {
+    const cache = await caches.open(cacheName);
+    return await cache.addAll(assetsToCache);
+  } catch (error) {
+    console.error('Failed to install assets cache: ', error);
+  }
+}
+
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing service worker...');
-  event.waitUntil(
-    caches.open('app-shell-v1')
-      .then(cache => {
-        return cache.addAll([
-          'offline.html'
-        ]);
-      })
-  );
+  event.waitUntil(cacheStaticAssets());
   self.skipWaiting();    
 });
 
+function removeOldCache(cacheKey) {
+  if(cacheKey !== cacheName) {
+    console.log('[Service Worker] Removing old cache ')
+    return caches.delete(cacheKey);
+  }
+}
+
+async function cacheCleanUp() {
+  const keyList = await caches.keys();
+  return Promise.all(keyList.map(removeOldCache));
+}
+
 self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating service worker...');
+  event.waitUntil(cacheCleanUp());
+  self.clients.claim();
 });
 
 async function networkFirst(request) {
   try {
     return await fetch(request);
   } catch {
-    const cache = await caches.open('app-shell-v1');
+    const cache = await caches.open(cacheName);
     return cache.match('offline.html');
   }
 }
